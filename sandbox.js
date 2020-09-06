291e9d7b1c7e654a97b94a9e4ebb05ce0377b8c9
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-06 19:18:59 +0700

    types any

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,10 @@
-//explicit types
-var characters;
-var age;
-var isLoggedIn;
-// age = "Luigi";
-age = 30;
-//arrays
-var ninjas;
-// let ninjas: string[] = [];
-// ninjas = [10, 23];
-ninjas = ["Yoshi", "Mario"];
-//union types
+var age = 25;
+age = true;
+age = "hello";
+age = { name: "Luigi" };
 var mixed = [];
-mixed.push("hello");
-mixed.push(20);
-// mixed.push(false);
-var uid;
-uid = "123";
-uid = 123;
-// objects
-var ninjaOne;
-ninjaOne = {
-    name: "Yoshi",
-    age: 30
-};
-var ninjaTwo;
-ninjaTwo = {
-    name: "Mario",
-    age: 30
-};
+mixed.push(5);
+mixed.push("Mario");
+mixed.push(true);
+var ninja;
+ninja = { name: "Yoshi", age: 25 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,42 +1,12 @@
-//explicit types
-let characters: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any = 25;
+age = true;
+age = "hello";
+age = { name: "Luigi" };
 
-// age = "Luigi";
-age = 30;
+let mixed: any[] = [];
+mixed.push(5);
+mixed.push("Mario");
+mixed.push(true);
 
-//arrays
-let ninjas: string[];
-// let ninjas: string[] = [];
-
-// ninjas = [10, 23];
-ninjas = ["Yoshi", "Mario"];
-
-//union types
-let mixed: (string | number)[] = [];
-
-mixed.push("hello");
-mixed.push(20);
-// mixed.push(false);
-
-let uid: string | number;
-uid = "123";
-uid = 123;
-
-// objects
-let ninjaOne: object;
-ninjaOne = {
-  name: "Yoshi",
-  age: 30,
-};
-
-let ninjaTwo: {
-  name: string;
-  age: number;
-};
-
-ninjaTwo = {
-  name: "Mario",
-  age: 30,
-};
+let ninja: { name: any; age: any };
+ninja = { name: "Yoshi", age: 25 };
