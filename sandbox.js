ff928311ae1da309ed2daa455eb096a299f97920
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-06 14:32:21 +0700

    type basics

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var character = "mario";
+var age = 30;
+var isBlackBelt = false;
+// character = 25;
+character = "luigi";
+// age = "hello";
+age = 25;
+// isBlackBelt = "yes";
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(2.5));
---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
+
+// character = 25;
+character = "luigi";
+
+// age = "hello";
+age = 25;
+
+// isBlackBelt = "yes";
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(2.5));
